e7266d20992d28038aa67626298149266bca3261
Gul Akberova <kbrovagul0@gmail.com>
2023-03-14 19:38:28 +0400

    vercel

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 // File Storage____________________________
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "piblic/assets");
+    cb(null, "public/assets");
   },
   filename: function (req, file, cb) {
     cb(null, file.originalname);
